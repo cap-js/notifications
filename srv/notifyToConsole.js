a651a4714fa78d4af070e95b42bbab280f2b4e20
Paunov <ivan.paunov@sap.com>
2023-09-27 14:48:40 +0300

    Adress comments

--- srv/notifyToConsole.js
+++ srv/notifyToConsole.js
@@ -1,5 +1,6 @@
 const NotificationService = require('./service');
-const { createNotification } = require("./../lib/utils");
+const { createNotification, readFile, doesKeyExist } = require("./../lib/utils");
+const { createNotificationTypesMap } = require("./../lib/notificationTypes");
 
 module.exports = class NotifyToConsole extends NotificationService {
   async init() {
@@ -8,8 +9,23 @@ module.exports = class NotifyToConsole extends NotificationService {
   }
 
   notify() {
+
     const notification = createNotification(arguments);
 
-    console.log(`SAP Alert Notification service notification: ${JSON.stringify(notification, null, 2)}`);
+    if (notification) {
+      console.log(`SAP Alert Notification service notification: ${JSON.stringify(notification, null, 2)}`);
+
+      const typesFile = readFile(cds.env.requires.notifications.types);
+      const existingTypes = createNotificationTypesMap(typesFile);
+  
+      if (!doesKeyExist(existingTypes, notification["NotificationTypeKey"])) {
+        console.log(`Notification Type ${notification["NotificationTypeKey"]} is not in the notification types file`);
+      }
+  
+      if (!doesKeyExist(existingTypes[notification["NotificationTypeKey"]], notification["NotificationTypeVersion"])) {
+        console.log(`Notification Type Version ${notification["NotificationTypeVersion"]} for type ${notification["NotificationTypeKey"]} is not in the notification types file`);
+      }
+    }
+    
   }
 }
