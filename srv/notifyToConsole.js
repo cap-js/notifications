d810851504868390595183ccf31a818f5ff23de4
Paunov <ivan.paunov@sap.com>
2023-09-27 12:47:44 +0300

    Address comments

--- srv/notifyToConsole.js
+++ srv/notifyToConsole.js
@@ -1,5 +1,5 @@
 const NotificationService = require('./service');
-const { getNotificationTypesKeyWithPrefix, createDefaultNotificationObject, POSSIBLE_PRIORITIES } = require("./../lib/utils");
+const { createNotification } = require("./../lib/utils");
 
 module.exports = class NotifyToConsole extends NotificationService {
   async init() {
@@ -8,29 +8,7 @@ module.exports = class NotifyToConsole extends NotificationService {
   }
 
   notify() {
-    let notification; 
-    
-    if (arguments.length == 1 && typeof arguments[0] === "object") {
-      notification = arguments[0];
-      notification["NotificationTypeKey"] = getNotificationTypesKeyWithPrefix(notification["NotificationTypeKey"]);
-  
-    } else {
-      let recipients = arguments[0];
-      let priority = arguments[1];
-      let title = arguments[2];
-      let description = arguments[3] ? arguments[3] : "";
-  
-      if (!POSSIBLE_PRIORITIES.includes(priority)) {
-        throw new Error(`Invalid priority ${priority}. Allowed priorities are LOW, NEUTRAL, MEDIUM, HIGH`);
-      }
-  
-      notification = createDefaultNotificationObject(
-        recipients,
-        priority,
-        title,
-        description
-      );
-    }
+    const notification = createNotification(arguments);
 
     console.log(`SAP Alert Notification service notification: ${JSON.stringify(notification, null, 2)}`);
   }
