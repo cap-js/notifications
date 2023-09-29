5733a90d2eace74c4dd26852d3c39ed9c32b7a1e
I524884 <sidakdeep.singh@sap.com>
2023-09-29 21:29:25 +0530

    update cloud-sdk to v3

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,8 +1,10 @@
 const cds = require("@sap/cds");
 const { validateNotificationTypes, readFile } = require("./lib/utils");
 const { createNotificationTypesMap, processNotificationTypes} = require("./lib/notificationTypes");
+const { setGlobalLogLevel } = require("@sap-cloud-sdk/util");
 
 cds.once("served", async () => {
+  setGlobalLogLevel("error");
   const profiles = cds.env.profiles ?? [];
   const production = profiles.includes('production');
 
