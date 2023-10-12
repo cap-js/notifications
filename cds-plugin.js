39402a4814efec6f104b9e835a2e6319c4df0997
I339886 <s.petkov@sap.com>
2023-10-12 14:10:52 +0300

    Deploy Notification Types within a CF Task

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,22 +1,4 @@
 const cds = require("@sap/cds");
-const { validateNotificationTypes, readFile } = require("./lib/utils");
-const { createNotificationTypesMap, processNotificationTypes} = require("./lib/notificationTypes");
 const { setGlobalLogLevel } = require("@sap-cloud-sdk/util");
 
-cds.once("served", async () => {
-  setGlobalLogLevel("error");
-  const profiles = cds.env.profiles ?? [];
-  const production = profiles.includes('production');
-
-  // read notification types
-  const notificationTypes = readFile(cds.env.requires?.notifications?.types);
-
-  if(validateNotificationTypes(notificationTypes)) {
-    if (production) {
-      await processNotificationTypes(notificationTypes);
-    } else {
-      const notificationTypesMap = createNotificationTypesMap(notificationTypes, true);
-      cds.notifications = { local: { types: notificationTypesMap }};
-    }
-  }
-});
+cds.once("served", async () => setGlobalLogLevel("error"));
