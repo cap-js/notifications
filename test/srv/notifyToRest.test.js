cbbb273cc63d7e6cf46b937e81b3ce04822959f3
I339886 <s.petkov@sap.com>
2023-10-23 15:21:58 +0300

    Add additional tests for notifyToRest

--- test/srv/notifyToRest.test.js
+++ test/srv/notifyToRest.test.js
@@ -1,20 +1,22 @@
-const NotifyToRest = require('../../srv/notifyToRest');
-const { messages } = require('../../lib/utils')
+const NotifyToRest = require("../../srv/notifyToRest");
+const { messages, buildNotification } = require("../../lib/utils");
+const { postNotification } = require("../../lib/notifications");
 
-describe('notify to rest', () => {
+jest.mock("./../../lib/notifications");
 
-  it('when no object is passed', async () => {
+describe("notify to rest", () => {
+  it("when no object is passed", async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
+    const warnSpy = jest.spyOn(global.console, "warn");
     notifyToRest.notify();
     expect(warnSpy).toHaveBeenCalled();
     expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.NO_OBJECT_FOR_NOTIFY);
     warnSpy.mockClear();
   });
 
-  it('when empty object is passed', async () => {
+  it("when empty object is passed", async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
+    const warnSpy = jest.spyOn(global.console, "warn");
     notifyToRest.notify({});
     expect(warnSpy).toHaveBeenCalled();
     expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.EMPTY_OBJECT_FOR_NOTIFY);
@@ -23,37 +25,48 @@ describe('notify to rest', () => {
 
   it(`when recipients or title isn't passed in default notification`, async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
+    const warnSpy = jest.spyOn(global.console, "warn");
     notifyToRest.notify({ dummy: true });
     expect(warnSpy).toHaveBeenCalled();
-    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.MANDATORY_PARAMETER_NOT_PASSED_FOR_DEFAULT_NOTIFICATION)
+    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.MANDATORY_PARAMETER_NOT_PASSED_FOR_DEFAULT_NOTIFICATION);
     warnSpy.mockClear();
   });
 
   it(`when title isn't a string in default notification`, async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
-    notifyToRest.notify({ title: 1, recipients: [ "abc@abc.com" ] });
+    const warnSpy = jest.spyOn(global.console, "warn");
+    notifyToRest.notify({ title: 1, recipients: ["abc@abc.com"] });
     expect(warnSpy).toHaveBeenCalled();
-    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.TITLE_IS_NOT_STRING)
+    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.TITLE_IS_NOT_STRING);
     warnSpy.mockClear();
   });
 
   it(`when priority isn't valid in default notification`, async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
-    notifyToRest.notify({ title: "abc", recipients: [ "abc@abc.com" ], priority: "abc" });
+    const warnSpy = jest.spyOn(global.console, "warn");
+    notifyToRest.notify({ title: "abc", recipients: ["abc@abc.com"], priority: "abc" });
     expect(warnSpy).toHaveBeenCalled();
-    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", "Invalid priority abc. Allowed priorities are LOW, NEUTRAL, MEDIUM, HIGH")
+    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", "Invalid priority abc. Allowed priorities are LOW, NEUTRAL, MEDIUM, HIGH");
     warnSpy.mockClear();
   });
 
   it(`when description isn't valid in default notification`, async () => {
     const notifyToRest = new NotifyToRest();
-    const warnSpy = jest.spyOn(global.console, 'warn');
-    notifyToRest.notify({ title: "abc", recipients: [ "abc@abc.com" ], priority: "low", description: true });
+    const warnSpy = jest.spyOn(global.console, "warn");
+    notifyToRest.notify({ title: "abc", recipients: ["abc@abc.com"], priority: "low", description: true });
     expect(warnSpy).toHaveBeenCalled();
-    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.DESCRIPTION_IS_NOT_STRING)
+    expect(warnSpy).toHaveBeenCalledWith("[notifications] -", messages.DESCRIPTION_IS_NOT_STRING);
     warnSpy.mockClear();
   });
-})
+
+  it(`When correct body is send | Then notification is posted`, async () => {
+    postNotification.mockImplementation(() => undefined);
+    const body = { title: "abc", recipients: ["abc@abc.com"], priority: "low" };
+
+    const notifyToRest = new NotifyToRest();
+    await notifyToRest.init();
+    await notifyToRest.notify(body);
+    expect(postNotification).toHaveBeenCalled();
+    expect(postNotification.mock.calls[0][0]).toMatchObject(buildNotification(body));
+  });
+});
