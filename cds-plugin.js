599b1e851e85e4362fa9288776046b9473184482
Theo Exall <texall@deloitte.co.za>
2024-09-09 14:34:06 +0200

    Test to see if I can get it working with CDS 8

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,11 +1,25 @@
 const cds = require("@sap/cds/lib");
+const { copy, exists, path } = cds.utils
 
 if (cds.cli.command === "build") {
   // register build plugin
-  cds.build?.register?.('notifications', { 
-      impl: '@cap-js/notifications/lib/build',
-      taskDefaults: { src: cds.env.folders.srv }
+  cds.build?.register?.('notifications', class NotificationsBuildPlugin extends cds.build.Plugin {
+
+    static taskDefaults = { src: cds.env.folders.srv }
+
+    static hasTask() {
+      const notificationTypesFile = cds.env.requires?.notifications?.types;
+      return notificationTypesFile === undefined ? false : exists(notificationTypesFile);
+    }
+
+    async build() {
+      if (exists(cds.env.requires.notifications?.types)) {
+        const fileName = path.basename(cds.env.requires.notifications.types);
+        await copy(cds.env.requires.notifications.types).to(path.join(this.task.dest, fileName));
+      }
+    }
   });
+  return;
 }
 
 else cds.once("served", async () => {
